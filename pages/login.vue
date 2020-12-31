2ac9ac23f0a5b3e4c92f3f7089742b56e187706e
KenyaSugimoto <45161125+KenyaSugimoto@users.noreply.github.com>
2020-12-31 15:01:19 +0900

    refactor: ログインページ(id,pass)のバックアップ

--- pages/login.vue
+++ pages/login.vue
@@ -1,5 +1,35 @@
 <template>
   <v-row>
-    <h1>Login page</h1>
+    <v-col cols="12">
+      <h1>Login page</h1>
+    </v-col>
+    <v-col>
+      <v-text-field v-model="id" label="id"></v-text-field>
+      <v-text-field
+        v-model="password"
+        label="password"
+        :type="showPass ? 'text' : 'password'"
+        :append-icon="showPass ? 'mdi-eye' : 'mdi-eye-off'"
+        @click:append="showPass = !showPass"
+      ></v-text-field>
+      <v-btn @click="goMainPage">ログイン</v-btn>
+    </v-col>
   </v-row>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      id: '',
+      password: '',
+      showPass: false,
+    }
+  },
+  methods: {
+    goMainPage() {
+      this.$router.push('/main')
+    },
+  },
+}
+</script>

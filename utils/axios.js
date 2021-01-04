8735dfac644525b6b0fabad43527d734d82733fc
KenyaSugimoto <45161125+KenyaSugimoto@users.noreply.github.com>
2021-01-04 11:30:10 +0900

    サーバのポート番号変更

--- utils/axios.js
+++ utils/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const instance = axios.create({
-  baseURL: 'http://localhost:5000/',
+  baseURL: 'http://localhost:50000/',
   headers: {
     'Access-Control-Allow-Origin': '*',
     'Content-Type': 'application/json',

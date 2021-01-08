19e391053577708744b4ac1aa2026de28ea57965
KenyaSugimoto <45161125+KenyaSugimoto@users.noreply.github.com>
2021-01-08 18:08:11 +0900

    add: ユーザステータスの要素の追加

--- components/UserStatus.vue
+++ components/UserStatus.vue
@@ -1,5 +1,27 @@
 <template>
-  <v-row>
-    <v-card>kenya</v-card>
-  </v-row>
+  <v-card>
+    <v-row>
+      <v-col>
+        <v-img :src="icon" height="40px" width="40px"></v-img>
+      </v-col>
+      <v-col>
+        <template>{{ userName }} さん</template>
+        <br />
+        <template>ランク: 砂</template>
+      </v-col>
+    </v-row>
+  </v-card>
 </template>
+
+<script>
+export default {
+  computed: {
+    icon() {
+      return this.$store.getters['user/photoURL']
+    },
+    userName() {
+      return this.$store.getters['user/userName']
+    },
+  },
+}
+</script>
